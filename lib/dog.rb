5a3098dd9d0001a13d24a4155f4e13414b1a069f
Daniel Matney <danieljmatney@gmail.com>
2019-12-08 07:43:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,8 @@ require_relative '../config/environment.rb'
 class Dog
   
   def initialize(hash)
-    self.id = nil
     hash.each {|key, value| self.send(("#{key}="), value)}
-    #self.id = nil
+    self.id = nil
   end
   
   
