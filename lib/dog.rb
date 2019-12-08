62fde62ae824636039b9a6a58092a5795f34febc
Daniel Matney <danieljmatney@gmail.com>
2019-12-08 07:38:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   
   def initialize(hash)
-    hash.each {|key, value| self.send(("#{key}="), value)}
+    #hash.each {|key, value| self.send(("#{key}="), value)}
     return hash
   end
   
