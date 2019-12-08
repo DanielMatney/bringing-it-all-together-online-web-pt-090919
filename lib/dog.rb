ac2ee855744c32dcedfac485691dd0ca3dd96844
Daniel Matney <danieljmatney@gmail.com>
2019-12-08 07:59:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,9 +10,13 @@ class Dog
   def create_table
     sql = <<-SQL
     "CREATE TABLE IF NOT EXISTS dogs(
-    id 
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
     )
     SQL
+    
+    
   end
   
   
